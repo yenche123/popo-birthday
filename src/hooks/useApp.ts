6a8961f92a85d151de93cc77b4e0d71f1499661b
yenche123 <tsuiyenche@outlook.com>
2024-10-13 10:59:26 +0800

    chore: ts config

--- src/hooks/useApp.ts
+++ src/hooks/useApp.ts
@@ -3,5 +3,9 @@ import { useGlobalLoading } from "./tools/useGlobalLoading";
 export function useApp() {
   
   useGlobalLoading()
+
+  // print version
+  const version = LIU_ENV.version
+  console.log(`当前版本: ${version}`)
   
 }
